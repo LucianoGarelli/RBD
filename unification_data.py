d036212513f61f3be34c3271b615ae764552623a
Luciano Garelli <lucianogarelli@gmail.com>
2022-02-08 14:37:01 -0300

    Add load and save directories

--- unification_data.py
+++ unification_data.py
@@ -13,7 +13,10 @@ time = np.linspace(t0,tf,N)
 # input data
 # alpha
 data = []
-data = np.loadtxt('alpha_BFP.csv', delimiter=',', skiprows=1)
+dir_save = './Resu_ref/Wernert_AIAA2010_7460/Caso_F01_unificated_V2/'
+dir_load = './Resu_ref/Wernert_AIAA2010_7460/'
+
+data = np.loadtxt(dir_load + 'alpha_BFP.csv', delimiter=',', skiprows=1)
 t_alpha = data[:,0]
 alpha = -1*data[:,1]
 # interpolated data
@@ -23,7 +26,7 @@ alpha_inter = np.deg2rad(alpha_inter)
 # input data
 # beta
 data = []
-data = np.loadtxt('beta_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load + 'beta_BFP.csv', delimiter=',', skiprows=1)
 t_beta = data[:,0]
 beta = -1*data[:,1]
 # interpolated data
@@ -33,7 +36,7 @@ beta_inter = np.deg2rad(beta_inter)
 # input data
 # alpha total
 data = []
-data = np.loadtxt('alpha_tot_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load + 'alpha_tot_BFP.csv', delimiter=',', skiprows=1)
 t_alpha_tot = data[:,0]
 alpha_tot = data[:,1]
 # interpolated data
@@ -42,7 +45,7 @@ alpha_tot_inter = np.interp(time, t_alpha_tot, alpha_tot)
 # input data
 # Force X tot
 data = []
-data = np.loadtxt('Forces_X_tot_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'Forces_X_tot_BFP.csv', delimiter=',', skiprows=1)
 t_Fxtot = data[:,0]
 Fxtot = data[:,1]
 # interpolated data
@@ -51,7 +54,7 @@ Fxtot_inter = np.interp(time, t_Fxtot, Fxtot)
 # input data
 # Force Gx
 data = []
-data = np.loadtxt('Forces_X_G_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'Forces_X_G_BFP.csv', delimiter=',', skiprows=1)
 t_Fxg = data[:,0]
 Fxg = data[:,1]
 # interpolated data
@@ -62,7 +65,7 @@ Fx = Fxtot_inter-Fxg_inter # hay que sustraerle el aporte de la gravedad, ya que
 # input data
 # Force Y tot
 data = []
-data = np.loadtxt('Forces_Y_tot_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'Forces_Y_tot_BFP.csv', delimiter=',', skiprows=1)
 t_Fytot = data[:,0]
 Fytot = data[:,1]
 # interpolated data
@@ -70,7 +73,7 @@ Fytot_inter = np.interp(time, t_Fytot, Fytot)
 
 # input data
 # Force Gy - es zero
-#data = np.loadtxt('Forces_Y_G_BFP.csv', delimiter=',', skiprows=1)
+#data = np.loadtxt(dir_load +'Forces_Y_G_BFP.csv', delimiter=',', skiprows=1)
 #t_Fyg = data[:,0]
 #Fyg = data[:,1]
 # interpolated data
@@ -83,7 +86,7 @@ Fy = Fytot_inter-Fyg_inter
 # input data
 # Force Z tot
 data = []
-data = np.loadtxt('Forces_Z_tot_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'Forces_Z_tot_BFP.csv', delimiter=',', skiprows=1)
 t_Fztot = data[:,0]
 Fztot = data[:,1]
 # interpolated data
@@ -92,7 +95,7 @@ Fztot_inter = np.interp(time, t_Fztot, Fztot)
 # input data
 # Force Gz
 data = []
-data = np.loadtxt('Forces_Z_G_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'Forces_Z_G_BFP.csv', delimiter=',', skiprows=1)
 t_Fzg = data[:,0]
 Fzg = data[:,1]
 # interpolated data
@@ -103,7 +106,7 @@ Fz = Fztot_inter-Fzg_inter
 # p -roll
 # spin rate BFP = spin rate BR
 data = []
-data = np.loadtxt('spin_rate_BR.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'spin_rate_BR.csv', delimiter=',', skiprows=1)
 t_p = data[:,0]
 p = data[:,1]
 # interpolated data
@@ -112,7 +115,7 @@ p_inter = 2.*np.pi*np.interp(time, t_p, p)
 
 # q - pith rate
 data = []
-data = np.loadtxt('pitch_rate_q_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'pitch_rate_q_BFP.csv', delimiter=',', skiprows=1)
 t_q = data[:,0]
 q = data[:,1]
 # interpolated data
@@ -121,7 +124,7 @@ q_inter = 2.*np.pi*np.interp(time, t_q, q)
 
 # r -yaw rate
 data = []
-data = np.loadtxt('yaw_rate_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'yaw_rate_BFP.csv', delimiter=',', skiprows=1)
 t_r = data[:,0]
 r = data[:,1]
 # interpolated data
@@ -130,7 +133,7 @@ r_inter = 2.*np.pi*np.interp(time, t_r, r)
 
 # vel "x" en NED
 data = []
-data = np.loadtxt('UE_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'UE_BFP.csv', delimiter=',', skiprows=1)
 t_vxned = data[:,0]
 vxned = data[:,1]
 # interpolated data
@@ -138,7 +141,7 @@ vxned_inter = np.interp(time, t_vxned, vxned)
 
 # vel "y" en NED
 data = []
-data = np.loadtxt('VE_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'VE_BFP.csv', delimiter=',', skiprows=1)
 t_vyned = data[:,0]
 vyned = data[:,1]
 # interpolated data
@@ -146,7 +149,7 @@ vyned_inter = np.interp(time, t_vyned, vyned)
 
 # vel "z" en NED
 data = []
-data = np.loadtxt('we_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'we_BFP.csv', delimiter=',', skiprows=1)
 t_vzned = data[:,0]
 vzned = data[:,1]
 # interpolated data
@@ -154,7 +157,7 @@ vzned_inter = np.interp(time, t_vzned, vzned)
 
 # Vel NED
 data = []
-data = np.loadtxt('mag_V_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'mag_V_BFP.csv', delimiter=',', skiprows=1)
 t_vel = data[:,0]
 vel = data[:,1]
 # interpolated data
@@ -189,7 +192,7 @@ for k, xk in enumerate(x):
 
 # x - recorrido longitud
 data = []
-data = np.loadtxt('XE_QFW.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'XE_QFW.csv', delimiter=',', skiprows=1)
 t_xe = data[:,0]
 xe = data[:,1]
 # interpolated data
@@ -197,7 +200,7 @@ xe_inter = 1000.*np.interp(time, t_xe, xe)
 
 # y - recorrido  lateral
 data = []
-data = np.loadtxt('YE_QFW.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'YE_QFW.csv', delimiter=',', skiprows=1)
 t_ye = data[:,0]
 ye = data[:,1]
 # interpolated data
@@ -206,7 +209,7 @@ ye_inter = np.interp(time, t_ye, ye)
 
 # z - altura
 data = []
-data = np.loadtxt('ZE_QFW.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'ZE_QFW.csv', delimiter=',', skiprows=1)
 t_ze = data[:,0]
 ze = data[:,1]
 # interpolated data
@@ -214,7 +217,7 @@ ze_inter = 1000*np.interp(time, t_ze, ze)
 
 # Moment X
 data = []
-data = np.loadtxt('Moment_X_tot_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'Moment_X_tot_BFP.csv', delimiter=',', skiprows=1)
 t_mx = data[:,0]
 mx = data[:,1]
 # interpolated data
@@ -222,7 +225,7 @@ mx_inter = np.interp(time, t_mx, mx)
 
 # Moment Y
 data = []
-data = np.loadtxt('Moment_Y_tot_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load +'Moment_Y_tot_BFP.csv', delimiter=',', skiprows=1)
 t_my = data[:,0]
 my = data[:,1]
 # interpolated data
@@ -230,30 +233,28 @@ my_inter = np.interp(time, t_my, my)
 
 # Moment Z
 data = []
-data = np.loadtxt('Moment_Z_tot_BFP.csv', delimiter=',', skiprows=1)
+data = np.loadtxt(dir_load + 'Moment_Z_tot_BFP.csv', delimiter=',', skiprows=1)
 t_mz = data[:,0]
 mz = data[:,1]
 # interpolated data
 mz_inter = np.interp(time, t_mz, mz)
 
-# Directory Set up (Resu): Data.hdf5, Forces_prop_unificated.txt, Moment_prop_unificated.txt
-MYDIR = ("Resultados")
-CHECK_FOLDER = os.path.isdir(MYDIR)
-
+CHECK_FOLDER = os.path.isdir(dir_save)
 # If folder doesn't exist, then create it.
 if not CHECK_FOLDER:
-    os.makedirs(MYDIR)
-    print("Directorio Resultados creado: ", MYDIR)
+    os.makedirs(dir_save)
+    print("Directorio creado: ", dir_save)
 else:
-    print(MYDIR, "Directorio Resultados existente.")
+    print(dir_save, "Directorio existente.")
 
 # File to write force - Body Frame
-ff = open("./Resultados/Forces_proc.txt", "w")
+
+ff = open(dir_save + 'Forces_proc.txt', 'w')
 ff.write("# Time,     alpha,     beta,     V_inf,    V_inf, V_inf, V_inf,  p, "
          "      q,         r,         gx,         gy,       gz,      FX,         FY,         FZ       \n")
 ff.close()
 
-ff = open('./Resultados/Forces_proc.txt', 'ab')
+ff = open( dir_save +'Forces_proc.txt', 'ab')
 #f_force = np.asarray([dt*(k+1), alfa, beta, vt, x[3], x[4], x[5], x[10], x[11], x[12],g_body[0],g_body[1],g_body[2], C_body[0],C_body[1],C_body[2]])
 f_force = np.asarray([time, alpha_inter, beta_inter, vel_inter, ue_inter, ve_inter, we_inter, p_inter, q_inter, r_inter, Fxg_inter, Fyg_inter, Fzg_inter, Fx, Fy, Fz])
 f_force = f_force.T
@@ -261,11 +262,11 @@ np.savetxt(ff, f_force, delimiter=", ", fmt='%1.3e')
 ff.close()
 
 # File to write moment - Body Frame
-fm = open("./Resultados/Moments_proc.txt", "w")
+fm = open(dir_save + 'Moments_proc.txt', 'w')
 fm.write("# Time,     alpha,      beta,     p,      q,      r,    MX,     MY,     MZ \n")
 fm.close()
 
-fm = open('./Resultados/Moments_proc.txt', 'ab')
+fm = open(dir_save + 'Moments_proc.txt', 'ab')
 # modificar este para momentos wind
 m_moment = np.asarray([time, alpha_inter, beta_inter, p_inter,  q_inter, r_inter, mx_inter, my_inter, mz_inter])
 m_moment = m_moment.T
@@ -330,7 +331,7 @@ plt.title('MZ BFP')
 plt.xlabel('Time [s]')
 
 #sv.save_data(N, time, x, Ixx, Iyy, Izz)
-sv.save_data(N-1, time, x, 0, 0, 0,"")
+sv.save_data(N-1, time, x, 0, 0, 0,dir_save)
 
 print('*-----------------------------------------------*')
 print('\nFIN! - OK.\n')
