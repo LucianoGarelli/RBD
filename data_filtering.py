51ada111977d1e0eb024ff83baa38fe1779f803e
Luciano Garelli <lucianogarelli@gmail.com>
2022-02-09 15:23:45 -0300

    Use input time vector for smoothing and add new MZ digital data

--- data_filtering.py
+++ data_filtering.py
@@ -4,15 +4,15 @@ from csaps import csaps
 from scipy.signal import savgol_filter
 
 #data = np.loadtxt('Resu_ref/Wernert_AIAA2010_7460/beta_BFP.csv', delimiter=',', skiprows=0)
-#data = np.loadtxt('./Resu_ref/Wernert_AIAA2010_7460/Caso_F01_unificated/Forces_proc.txt', delimiter=',', skiprows=1)
-data = np.loadtxt('./Resu_ref/Wernert_AIAA2010_7460/Caso_F01_unificated/Moments_proc.txt', delimiter=',', skiprows=1)
+data = np.loadtxt('./Resu_ref/Wernert_AIAA2010_7460/Caso_F01_unificated/Forces_proc.txt', delimiter=',', skiprows=1)
+#data = np.loadtxt('./Resu_ref/Wernert_AIAA2010_7460/Caso_F01_unificated/Moments_proc.txt', delimiter=',', skiprows=1)
 
 uniqueValues, indicesList = np.unique(data[:,0], return_index=True)
 data_raw = data[indicesList,:]
-i=8
+i=15
 
 xs = np.linspace(data_raw[0,0], data_raw[-1,0], 5000)
-ys = csaps(data_raw[:,0], data_raw[:,i], xs, smooth=0.9995)
+ys = csaps(data_raw[:,0], data_raw[:,i], data_raw[:,0], smooth=0.999)
 smoothing_result = csaps(data_raw[:,0], data_raw[:,i], xs)
 
 yhat = savgol_filter(data_raw[:,i], 51, 5)
@@ -24,7 +24,7 @@ fig.canvas.set_window_title('Datos')
 fig.suptitle('Datos filtrados')
 plt.plot(data_raw[:,0], data_raw[:,i])
 plt.plot(data_raw[:,0],yhat, color='green')
-plt.plot(xs, ys, color='red')
+plt.plot(data_raw[:,0], ys, color='red')
 
 plt.grid()
 
